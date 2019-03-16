85fd2cdf0916a5d8e47795af3822894c3df7d64d
lucasdosenai <39661894+lucasdosenai@users.noreply.github.com>
2019-03-16 20:06:35 -0300

    tudo que fiz antes de buga tudo

--- src/main/java/DAO/UsuarioDAO.java
+++ src/main/java/DAO/UsuarioDAO.java
@@ -18,41 +18,15 @@ public class UsuarioDAO {
 		con = ConDB.getConnection();
 	}
 	
-	public List<Usuario> user_not_repiter() throws SQLException{
-		List<Usuario> usuariosEncontrado = new ArrayList<Usuario>();
-		String SQL = "SELECT usuario as u, email as e FROM usuario";
-		int numeroRegistros = 0;
-
-			PreparedStatement ps = con.prepareStatement(SQL);
-			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
-				numeroRegistros++;
-				
-				Usuario u = new Usuario();
-				u.setEmail(rs.getString("e"));
-				u.setUsuario(rs.getString("u"));
-				usuariosEncontrado.add(u);
-			}
-			System.out.println(numeroRegistros + " ENCONTRADO! [ user_not_repiter ]");
-			return usuariosEncontrado;
-	}
+	
 	
 	public boolean cadastrarUsuario(Usuario u) throws SQLException {
 		PreparedStatement ps;
-		List<Usuario> usuariosRetornado = user_not_repiter();
-		if(usuariosRetornado != null) {
-			for(Usuario uCompara : usuariosRetornado) {
-				if(uCompara.getEmail().equals(u.getEmail()) && uCompara.getUsuario().equals(u.getUsuario())) {
-					System.out.println("Usuario/Email já existe no sistema, verifique!");
-					return false;
-				}else {
 					String SQL_GERA_PERMISSAO = "INSERT INTO permissao(codigo) VALUE codigo = ";
 					String SQL_BUSCA_ID = "SELECT codigo as c FROM usuario WHERE "
-							+ "usuario = " + u.getUsuario() + " AND "
-							+ "nome = " + u.getNome() + " AND "
-							+ "sobrenome = " + u.getSobrenome() + " AND "
-							+ "email = " + u.getEmail() + " AND "
-							+ "senha = " + u.getSenha();
+							+ "usuario = " + u.getUsuario().toString() + " AND "
+							+ "email = " + u.getEmail().toString()+";"; 
+					
 					System.out.println(SQL_GERA_PERMISSAO);
 					System.out.println(SQL_BUSCA_ID);
 					String SQL = "INSERT INTO usuario(usuario,nome,sobrenome,email,senha) VALUE("
@@ -64,21 +38,16 @@ public class UsuarioDAO {
 					System.out.println(SQL);
 					 ps = con.prepareStatement(SQL);
 					if(ps.executeUpdate() > 0) {
-						 ps = con.prepareStatement(SQL_BUSCA_ID);
-						 ResultSet rs = ps.executeQuery();
+						 PreparedStatement ps1 = con.prepareStatement(SQL_BUSCA_ID);
+						 ResultSet rs = ps1.executeQuery();
 						 String indentificadorEncontrado = rs.getString("c");
-						 ps = con.prepareStatement(SQL_GERA_PERMISSAO + indentificadorEncontrado);
+						 ps = con.prepareStatement(SQL_GERA_PERMISSAO + "'" +indentificadorEncontrado + "'");
 						 return ps.executeUpdate() > 0;
 						}else {
-							System.out.println("Problema ao inserir usuario");
+							System.out.println("Email/usuario já existe no sistema!");
+							return false;
 						}
-				} // FECHA ELSE
-			} // Fecha FOR
-		}else {
-			ps = con.prepareStatement("INSERT INTO usuario(usuario,nome,sobrenome,email,senha) values"
-					+ "('Adm','Adm','Sistema','ti.suporte@gmail.com','admadm')");
-			return ps.executeUpdate() > 0;
-		}
-		return false;
-	}
+			} // Fecha metodo
+
+		
 }
