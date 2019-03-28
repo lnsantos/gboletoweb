570bd603dc8a1336733b68643680b960ecb071da
lucasdosenai <39661894+lucasdosenai@users.noreply.github.com>
2019-03-28 13:15:01 -0300

    TentandoFazerUmasCoisas

---
+++ src/main/java/util/MegaMenuView.java
@@ -0,0 +1,16 @@
+package util;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class MegaMenuView {
+	private String orientation = "horizontal";
+	 
+    public String getOrientation() {
+        return orientation;
+    }
+ 
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+}
